4def8b102fa5a20f467c50f88aadddc2a40f8213
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2021-11-10 12:57:49 -0500

    flex dimensions

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const NewText = () => {
+const NewText = ({ style }) => {
 	const [text, setText] = useState(true);
 	const updateText = () => {
 		setText(!text)
 	}
-	return <Text style={styles.text} onPress={updateText} >
+	return <Text style={[ styles.text, style ]} onPress={ updateText } >
 		{text ? "Hola mundo" : "Hasta luego mundo" }
 	</Text>
 }
@@ -14,15 +14,32 @@ const NewText = () => {
 export default function App() {
   return (
     <View style={styles.container}>
-      <NewText />
+      <NewText style={styles.red} />
+      <NewText style={styles.green} />
+      <NewText style={styles.blue} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
 	text: {
-		color: 'green',
-		fontSize: 48,
+		color: 'white',
+		fontSize: 24,
+		height: 100,
+		width: 100,
+		backgroundColor: 'grey'
+	},
+	red: {
+		backgroundColor: 'red',
+		flex: 3,
+	},
+	green: {
+		backgroundColor: 'green',
+		flex: 2,
+	},
+	blue: {
+		backgroundColor: 'blue',
+		flex: 1,
 	},
   container: {
     flex: 1,
