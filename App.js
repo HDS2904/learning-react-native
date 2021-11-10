4bbcb183991fa617e27aee098eef70b0808a9758
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2021-11-10 13:06:49 -0500

    Distribución del contenido con flex

--- App.js
+++ App.js
@@ -25,26 +25,22 @@ const styles = StyleSheet.create({
 	text: {
 		color: 'white',
 		fontSize: 24,
-		height: 100,
-		width: 100,
 		backgroundColor: 'grey'
 	},
 	red: {
 		backgroundColor: 'red',
-		flex: 3,
 	},
 	green: {
 		backgroundColor: 'green',
-		flex: 2,
 	},
 	blue: {
 		backgroundColor: 'blue',
-		flex: 1,
 	},
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+		flexDirection: 'column',
+    justifyContent: 'space-around',
   },
 });
