6c58e63375ec797269e402fe7f11c9679eab3505
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2021-11-11 10:19:17 -0500

    ScrollView

--- App.js
+++ App.js
@@ -1,65 +1,118 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+import { Button, Dimensions, ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
 	const [newText, setNewText] = useState('');
 	const [submit, setSubmit] = useState('')
   return (
     <View style={styles.container}>
-      <Text>Texto: {submit}</Text>
-      <TextInput 
-				style={styles.input}
-				placeholder = 'Ingrese un texto'
-				onChangeText={i => setNewText(i)}
-				defaultValue = {newText}
-			/>
-			{/* <TouchableHighlight
-				underlayColor={'#999'}
-				activeOpacity={0.2}
-				title='Texto enviado con exito'
-				onPress={()=>{
-					setSubmit(newText);
-					alert('Texto enviado con exito')
-				}}
-			/> */}
-			{/* <TouchableNativeFeedback
-				background={TouchableNativeFeedback.Ripple('#00f',true)}
-				onPress={()=>{
-					setSubmit(newText);
-					alert('Texto enviado con exito')
-				}}
-			>
-				<View style={styles.view}>
-					<Text>Aceptar</Text>
-				</View>
-			</TouchableNativeFeedback> */}
-			{/* <TouchableOpacity
-				style={styles.TouchableOpacity}
-				onPress={()=>{
-					setSubmit(newText);
-					alert('Texto enviado con exito')
-				}}
-			>
-				<View style={styles.view}>
-					<Text>Aceptar</Text>
-				</View>
-			</TouchableOpacity> */}
-			<TouchableWithoutFeedback
-				style={styles.TouchableOpacity}
-				onPress={()=>{
-					setSubmit(newText);
-					alert('Texto enviado con exito')
-				}}
-			>
-				<View style={styles.view}>
-					<Text>Aceptar</Text>
-				</View>
-			</TouchableWithoutFeedback>
+			<ScrollView style={styles.ScrollView}>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<Text>Texto: {submit}</Text>
+				<TextInput 
+					style={styles.input}
+					placeholder = 'Ingrese un texto'
+					onChangeText={i => setNewText(i)}
+					defaultValue = {newText}
+				/>
+				{/* <TouchableHighlight
+					underlayColor={'#999'}
+					activeOpacity={0.2}
+					title='Texto enviado con exito'
+					onPress={()=>{
+						setSubmit(newText);
+						alert('Texto enviado con exito')
+					}}
+				/> */}
+				{/* <TouchableNativeFeedback
+					background={TouchableNativeFeedback.Ripple('#00f',true)}
+					onPress={()=>{
+						setSubmit(newText);
+						alert('Texto enviado con exito')
+					}}
+				>
+					<View style={styles.view}>
+						<Text>Aceptar</Text>
+					</View>
+				</TouchableNativeFeedback> */}
+				{/* <TouchableOpacity
+					style={styles.TouchableOpacity}
+					onPress={()=>{
+						setSubmit(newText);
+						alert('Texto enviado con exito')
+					}}
+				>
+					<View style={styles.view}>
+						<Text>Aceptar</Text>
+					</View>
+				</TouchableOpacity> */}
+				<TouchableWithoutFeedback
+					style={styles.TouchableOpacity}
+					onPress={()=>{
+						setSubmit(newText);
+						alert('Texto enviado con exito')
+					}}
+				>
+					<View style={styles.view}>
+						<Text>Aceptar</Text>
+					</View>
+				</TouchableWithoutFeedback>
+			</ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+	ScrollView: {
+		width: width
+	},
 	TouchableOpacity: {
 		backgroundColor: '#EEE',
 	},
