4bc77061fbe48cc47088d6ec3d2884872ac58270
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2021-11-11 12:42:19 -0500

    consumption of APIS

--- App.js
+++ App.js
@@ -1,57 +1,39 @@
-import React, { useState } from 'react';
-import { SectionList, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, SectionList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+	const [users, setUsers] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(resp => resp.json())
+			.then(data => {
+				setUsers(data);
+				setLoading(false);
+			})
+	}, [])
+
   return (
     <View style={styles.container}>
-			<SectionList
-				sections={[
-					{
-						title: 'Grupo 1',
-						data: [
-							{key: '1', name: 'María'},
-							{key: '2', name: 'Carlos'},
-							{key: '3', name: 'Jonathan'},
-							{key: '4', name: 'José'},
-						]
-					},
-					{
-						title: 'Grupo 2',
-						data: [
-							{key: '6', name: 'Ruso'},
-							{key: '7', name: 'Angel'},
-							{key: '8', name: 'Joel'},
-							{key: '9', name: 'Sergio'},
-						]
-					},
-					{
-						title: 'Grupo 3',
-						data: [
-							{key: '10', name: 'Valentín'},
-							{key: '11', name: 'Manuel'},
-							{key: '12', name: 'Juan'},
-							{key: '13', name: 'Joaquín'},
-						]
-					},
-					{
-						title: 'Grupo 4',
-						data: [
-							{key: '14', name: 'Steve'},
-							{key: '15', name: 'Julio'},
-							{key: '16', name: 'Lucía'},
-							{key: '17', name: 'Cesar'},
-							{key: '18', name: 'Monica'},
-						]
-					},
-				]}
-				renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
-				renderSectionHeader={({section}) => <Text style={styles.section}>{section.title}</Text>}
-			/>
+			{ loading
+				?<Text style={styles.center}>Loading...</Text>
+				:<FlatList
+					data={users}
+					renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+					keyExtractor={item=>`${item.id}`}
+				/>
+			}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
   container: {
 		paddingTop: 40,
     flex: 1,
@@ -65,10 +47,4 @@ const styles = StyleSheet.create({
 		borderBottomColor: '#ccc',
 		borderBottomWidth: 1
 	},
-	section: {
-		fontSize: 20,
-		backgroundColor: '#eee',
-		fontWeight: 'bold',
-		padding: 10
-	}
 });
