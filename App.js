68df3155804d88fd19ea84557574a001bc253525
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2021-11-11 11:02:26 -0500

    SectionList

--- App.js
+++ App.js
@@ -1,138 +1,74 @@
 import React, { useState } from 'react';
-import { Button, Dimensions, ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
-
-const { height, width } = Dimensions.get('window');
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-	const [newText, setNewText] = useState('');
-	const [submit, setSubmit] = useState('')
   return (
     <View style={styles.container}>
-			<ScrollView style={styles.ScrollView}>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<Text>Texto: {submit}</Text>
-				<TextInput 
-					style={styles.input}
-					placeholder = 'Ingrese un texto'
-					onChangeText={i => setNewText(i)}
-					defaultValue = {newText}
-				/>
-				{/* <TouchableHighlight
-					underlayColor={'#999'}
-					activeOpacity={0.2}
-					title='Texto enviado con exito'
-					onPress={()=>{
-						setSubmit(newText);
-						alert('Texto enviado con exito')
-					}}
-				/> */}
-				{/* <TouchableNativeFeedback
-					background={TouchableNativeFeedback.Ripple('#00f',true)}
-					onPress={()=>{
-						setSubmit(newText);
-						alert('Texto enviado con exito')
-					}}
-				>
-					<View style={styles.view}>
-						<Text>Aceptar</Text>
-					</View>
-				</TouchableNativeFeedback> */}
-				{/* <TouchableOpacity
-					style={styles.TouchableOpacity}
-					onPress={()=>{
-						setSubmit(newText);
-						alert('Texto enviado con exito')
-					}}
-				>
-					<View style={styles.view}>
-						<Text>Aceptar</Text>
-					</View>
-				</TouchableOpacity> */}
-				<TouchableWithoutFeedback
-					style={styles.TouchableOpacity}
-					onPress={()=>{
-						setSubmit(newText);
-						alert('Texto enviado con exito')
-					}}
-				>
-					<View style={styles.view}>
-						<Text>Aceptar</Text>
-					</View>
-				</TouchableWithoutFeedback>
-			</ScrollView>
+			<SectionList
+				sections={[
+					{
+						title: 'Grupo 1',
+						data: [
+							{key: '1', name: 'María'},
+							{key: '2', name: 'Carlos'},
+							{key: '3', name: 'Jonathan'},
+							{key: '4', name: 'José'},
+						]
+					},
+					{
+						title: 'Grupo 2',
+						data: [
+							{key: '6', name: 'Ruso'},
+							{key: '7', name: 'Angel'},
+							{key: '8', name: 'Joel'},
+							{key: '9', name: 'Sergio'},
+						]
+					},
+					{
+						title: 'Grupo 3',
+						data: [
+							{key: '10', name: 'Valentín'},
+							{key: '11', name: 'Manuel'},
+							{key: '12', name: 'Juan'},
+							{key: '13', name: 'Joaquín'},
+						]
+					},
+					{
+						title: 'Grupo 4',
+						data: [
+							{key: '14', name: 'Steve'},
+							{key: '15', name: 'Julio'},
+							{key: '16', name: 'Lucía'},
+							{key: '17', name: 'Cesar'},
+							{key: '18', name: 'Monica'},
+						]
+					},
+				]}
+				renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+				renderSectionHeader={({section}) => <Text style={styles.section}>{section.title}</Text>}
+			/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-	ScrollView: {
-		width: width
-	},
-	TouchableOpacity: {
-		backgroundColor: '#EEE',
-	},
-	view: {
-		height: 40,
-		width: 300,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	input: {
-		height: 42,
-		borderBottomColor: '#ccc',
-		borderBottomWidth: 1,
-		marginTop: 50,
-		width: '50%',
-	},
   container: {
+		paddingTop: 40,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+	item: {
+		padding: 10,
+		fontSize: 22,
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1
+	},
+	section: {
+		fontSize: 20,
+		backgroundColor: '#eee',
+		fontWeight: 'bold',
+		padding: 10
+	}
 });
