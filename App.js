c40825fffbdab881480b2336bbe73d2e6415e1f5
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2021-11-12 01:51:26 -0500

    ImageBackground

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image
+      <ImageBackground
 				style={styles.photo}
 				source={{uri: 'http://placekitten.com/200/300'}}
-			/>
+			>
+				<Text>Image Tests</Text>
+			</ImageBackground>
     </View>
   );
 }
